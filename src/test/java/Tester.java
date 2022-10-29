d6ae2ceb0e3314b89397ae292c65f0915a4de4c0
CameronHumphreysGIT <Cam117version@gmail.com>
2022-10-28 22:49:41 -0400

    A TEST commit
    line 47 acceptance test, currently runs with no code changes, consider REFACTOR to consolidate 45, 46, and 47.

--- src/test/java/Tester.java
+++ src/test/java/Tester.java
@@ -396,6 +396,30 @@ public class Tester {
             //teardown
             p.close();
         }
+        @Test
+        @DisplayName("47PlayerOneTurnRerollTest")
+        void FortySevenTest() {
+            Player p = new Player(Config.PLAYER_PORT_NUMBER);
+            //setup according to line 46
+            ArrayList<String> setup = new ArrayList<>(Arrays.asList("PARROT", "SKULL", "PARROT", "SWORD", "PARROT", "SWORD", "SKULL", "PARROT"));
+            setupSinglePlayer(p, setup);
+            //do reRolls (swords)
+            p.reRoll("35");
+            System.out.println("Initial reRoll:");
+            p.displayDice();
+            //now set the dice again
+            p.setDice(new ArrayList<>(Arrays.asList("PARROT", "SKULL", "PARROT", "SKULL", "PARROT", "SWORD", "SKULL", "PARROT")));
+            System.out.println("Setup:");
+            p.displayDice();
+            //now simulate server response and endturn
+            serverResponseDice(p);
+            //Server Score message is the word response the server gives with a given initial and final score, which should be zero since we rerolled and had three skulls
+            assertEquals(Config.SERVER_SCORE_MESSAGE(0, 0), p.getLastMessage());
+            //shouldn't be the player's turn anymore
+            assertFalse(p.getTurn());
+            //teardown
+            p.close();
+        }
     }
 
     void setupSinglePlayer(Player p, ArrayList<String> dice) {
