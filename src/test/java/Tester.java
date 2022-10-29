b3b9879f2e446f63afe726feb424e472eedc4fc5
CameronHumphreysGIT <Cam117version@gmail.com>
2022-10-29 18:18:49 -0400

    REFACTOR
    clearing logs and changing for correction grid

--- src/test/java/Tester.java
+++ src/test/java/Tester.java
@@ -569,12 +569,12 @@ public class Tester {
         void FiftyFiveTest() {
             Player p = new Player(Config.PLAYER_PORT_NUMBER);
             //setup according to line 55
-            ArrayList<String> setup = new ArrayList<>(Arrays.asList("PARROT", "PARROT", "MONKEY", "SWORD", "PARROT", "MONKEY", "SKULL", "MONKEY"));
+            ArrayList<String> setup = new ArrayList<>(Arrays.asList("DIAMOND", "DIAMOND", "SWORD", "DIAMOND", "PARROT", "SKULL", "SKULL", "MONKEY"));
             setupSinglePlayer(p, setup, Config.FORTUNE_CARDS.get(7));
             //now simulate server response and endturn
             serverResponseDice(p, Config.FORTUNE_CARDS.get(7));
             //Server Score message is the word response the server gives with a given initial and final score, which should be zero since we rerolled and had three skulls
-            assertEquals(Config.SERVER_SCORE_MESSAGE(0, 300), p.getLastMessage());
+            assertEquals(Config.SERVER_SCORE_MESSAGE(0, 500), p.getLastMessage());
             System.out.println(p.getLastMessage());
             //shouldn't be the player's turn anymore
             assertFalse(p.getTurn());
