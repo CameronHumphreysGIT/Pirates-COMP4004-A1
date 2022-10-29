715e1b59758feabb40f62102585545d63dece942
CameronHumphreysGIT <Cam117version@gmail.com>
2022-10-29 17:25:08 -0400

    A TEST commit
    acceptance test according to line 58, runs on first pass

--- src/test/java/Tester.java
+++ src/test/java/Tester.java
@@ -615,6 +615,23 @@ public class Tester {
             //teardown
             p.close();
         }
+        @Test
+        @DisplayName("58PlayerOneTurnRerollTest")
+        void FiftyEightTest() {
+            Player p = new Player(Config.PLAYER_PORT_NUMBER);
+            //setup according to line 58
+            ArrayList<String> setup = new ArrayList<>(Arrays.asList("SWORD", "SWORD", "PARROT", "PARROT", "SWORD", "PARROT", "SKULL", "PARROT"));
+            setupSinglePlayer(p, setup, Config.FORTUNE_CARDS.get(7));
+            //now simulate server response and endturn
+            serverResponseDice(p, Config.FORTUNE_CARDS.get(7));
+            //Server Score message is the word response the server gives with a given initial and final score, which should be zero since we rerolled and had three skulls
+            assertEquals(Config.SERVER_SCORE_MESSAGE(0, 400), p.getLastMessage());
+            System.out.println(p.getLastMessage());
+            //shouldn't be the player's turn anymore
+            assertFalse(p.getTurn());
+            //teardown
+            p.close();
+        }
     }
 
     //Helpers
