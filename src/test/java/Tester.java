9b27f89610be9c817906d30dc607bab7176f4320
CameronHumphreysGIT <Cam117version@gmail.com>
2022-10-29 11:22:05 -0400

    A TEST commit
    acceptance test for line 48 of grid, test passes without additional code

--- src/test/java/Tester.java
+++ src/test/java/Tester.java
@@ -420,6 +420,38 @@ public class Tester {
             //teardown
             p.close();
         }
+        @Test
+        @DisplayName("48PlayerOneTurnRerollTest")
+        void FortyEightTest() {
+            Player p = new Player(Config.PLAYER_PORT_NUMBER);
+            //setup according to line 46
+            ArrayList<String> setup = new ArrayList<>(Arrays.asList("PARROT", "SKULL", "PARROT", "SWORD", "PARROT", "SWORD", "SWORD", "PARROT"));
+            setupSinglePlayer(p, setup);
+            //do reRolls (swords)
+            p.reRoll("356");
+            System.out.println("Initial reRoll:");
+            p.displayDice();
+            //now set the dice again
+            p.setDice(new ArrayList<>(Arrays.asList("PARROT", "SKULL", "PARROT", "SKULL", "PARROT", "MONKEY", "MONKEY", "PARROT")));
+            System.out.println("Setup:");
+            p.displayDice();
+            //do reRolls (Monkeys)
+            p.reRoll("56");
+            System.out.println("Initial reRoll:");
+            p.displayDice();
+            //now set the dice again
+            p.setDice(new ArrayList<>(Arrays.asList("PARROT", "SKULL", "PARROT", "SKULL", "PARROT", "SKULL", "MONKEY", "PARROT")));
+            System.out.println("Setup:");
+            p.displayDice();
+            //now simulate server response and endturn
+            serverResponseDice(p);
+            //Server Score message is the word response the server gives with a given initial and final score, which should be zero since we rerolled and had three skulls
+            assertEquals(Config.SERVER_SCORE_MESSAGE(0, 0), p.getLastMessage());
+            //shouldn't be the player's turn anymore
+            assertFalse(p.getTurn());
+            //teardown
+            p.close();
+        }
     }
 
     void setupSinglePlayer(Player p, ArrayList<String> dice) {
