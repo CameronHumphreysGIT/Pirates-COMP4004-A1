fc693cc832d3da4d88b845c187b6aaaa8dea69df
CameronHumphreysGIT <Cam117version@gmail.com>
2022-10-30 15:30:16 -0400

    A TEST commit
    acceptance of line 69 (nice) currently passes, no code changes needed

--- src/test/java/Tester.java
+++ src/test/java/Tester.java
@@ -735,6 +735,22 @@ public class Tester {
             //teardown
             p.close();
         }
+        @Test
+        @DisplayName("69PlayerOneTurnRerollTest")
+        void SixtyNineTest() {
+            Player p = new Player(Config.PLAYER_PORT_NUMBER);
+            //setup according to line 69
+            ArrayList<String> setup = new ArrayList<>(Arrays.asList("MONKEY", "SWORD", "PARROT", "DIAMOND", "SWORD", "MONKEY", "SKULL", "SKULL"));
+            //this will do the second reroll
+            oneReRollTest(p, setup, new ArrayList<>(Arrays.asList("DIAMOND", "SWORD", "PARROT", "DIAMOND", "SWORD", "DIAMOND", "SKULL", "SKULL")), "23", 7);
+            //Server Score message is the word response the server gives with a given initial and final score, which should be zero since we rerolled and had three skulls
+            assertEquals(Config.SERVER_SCORE_MESSAGE(0, 500), p.getLastMessage());
+            System.out.println(p.getLastMessage());
+            //shouldn't be the player's turn anymore
+            assertFalse(p.getTurn());
+            //teardown
+            p.close();
+        }
     }
 
     //Helpers
