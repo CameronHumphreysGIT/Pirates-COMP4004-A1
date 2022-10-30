fea4d44539e833d80fc2f0ec7c8660b11b20b5a0
CameronHumphreysGIT <Cam117version@gmail.com>
2022-10-30 17:56:31 -0400

    A TEST commit
    line 84 test for monkey card, currently passes.

--- src/test/java/Tester.java
+++ src/test/java/Tester.java
@@ -901,6 +901,22 @@ public class Tester {
                     //teardown
                     p.close();
                 }
+                @Test
+                @DisplayName("84MonkeyTest")
+                void EightyFourTest() {
+                    Player p = new Player(Config.PLAYER_PORT_NUMBER);
+                    //setup according to line 83
+                    ArrayList<String> setup = new ArrayList<>(Arrays.asList("MONKEY", "PARROT", "SKULL", "MONKEY", "PARROT", "SKULL", "MONKEY", "SKULL"));
+                    //this will do the second reroll
+                    noReRollTest(p, setup,9);
+                    //Server Score message is the word response the server gives with a given initial and final score, which should be zero since we rerolled and had three skulls
+                    assertEquals(Config.SERVER_SCORE_MESSAGE(0, 0), p.getLastMessage());
+                    System.out.println(p.getLastMessage());
+                    //shouldn't be the player's turn anymore
+                    assertFalse(p.getTurn());
+                    //teardown
+                    p.close();
+                }
             }
         }
     }
