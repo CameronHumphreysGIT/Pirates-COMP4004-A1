d5023caf54a255dd0f3ad480804f79cc7d5ae7a0
CameronHumphreysGIT <Cam117version@gmail.com>
2022-10-30 15:34:10 -0400

    A TEST commit
    Acceptance of line 70, currently passes, noticed an error on test 69(nice), it passes, but the mock rerolls aren't correct... to be fixed

--- src/test/java/Tester.java
+++ src/test/java/Tester.java
@@ -751,6 +751,22 @@ public class Tester {
             //teardown
             p.close();
         }
+        @Test
+        @DisplayName("70PlayerOneTurnRerollTest")
+        void SeventyTest() {
+            Player p = new Player(Config.PLAYER_PORT_NUMBER);
+            //setup according to line 70
+            ArrayList<String> setup = new ArrayList<>(Arrays.asList("MONKEY", "SWORD", "PARROT", "SWORD", "SWORD", "GOLD", "SKULL", "GOLD"));
+            //this will do the second reroll
+            oneReRollTest(p, setup, new ArrayList<>(Arrays.asList("MONKEY", "PARROT", "PARROT", "GOLD", "MONKEY", "GOLD", "SKULL", "GOLD")), "134", 7);
+            //Server Score message is the word response the server gives with a given initial and final score, which should be zero since we rerolled and had three skulls
+            assertEquals(Config.SERVER_SCORE_MESSAGE(0, 600), p.getLastMessage());
+            System.out.println(p.getLastMessage());
+            //shouldn't be the player's turn anymore
+            assertFalse(p.getTurn());
+            //teardown
+            p.close();
+        }
     }
 
     //Helpers
