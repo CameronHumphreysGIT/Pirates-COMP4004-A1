3e92cb70ce5a6cd63a945556a5d39871717aa264
CameronHumphreysGIT <Cam117version@gmail.com>
2022-10-30 22:31:27 -0400

    A TEST commit
    line 118, currently runs, no code necessary.

--- src/test/java/Tester.java
+++ src/test/java/Tester.java
@@ -1232,6 +1232,23 @@ public class Tester {
                     //teardown
                     p.close();
                 }
+                @Test
+                @DisplayName("118BattleTest")
+                void OneHundredEighteenTest() {
+                    Player p = new Player(Config.PLAYER_PORT_NUMBER);
+                    //setup according to line 118
+                    ArrayList<String> setup = new ArrayList<>(Arrays.asList("PARROT", "SWORD", "MONKEY", "MONKEY", "MONKEY", "SKULL", "PARROT", "MONKEY"));
+                    //player is in skull island, start all player's with score of 1000
+                    oneReRollTest(p, setup, new ArrayList<>(Arrays.asList("SWORD", "SWORD", "MONKEY", "MONKEY", "MONKEY", "SKULL", "SKULL", "MONKEY")), "06", 4);
+                    //Server Score message is the word response the server gives with a given initial and final score, which should be zero since we rerolled and had three skulls
+                    assertEquals(Config.SERVER_SCORE_MESSAGE(0, 500), p.getLastMessage());
+                    //asserts on each player's score are outside done in functions called by twoReRollTest
+                    System.out.println(p.getLastMessage());
+                    //shouldn't be the player's turn anymore
+                    assertFalse(p.getTurn());
+                    //teardown
+                    p.close();
+                }
             }
         }
     }
